3f70f84c5cdbd51a07a2894ac46dba76850bac0e
jeremy <nice.memory@hotmail.fr>
2021-01-18 17:47:13 +0400

    tabs

---
+++ app/src/main/java/com/instabus/ui/main/MyStationsListRecyclerViewAdapter.kt
@@ -0,0 +1,42 @@
+package com.instabus.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.instabus.R
+
+import com.instabus.dummy.DummyContent.DummyItem
+
+/**
+ * [RecyclerView.Adapter] that can display a [DummyItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class MyStationsListRecyclerViewAdapter(
+        private val values: List<DummyItem>)
+    : RecyclerView.Adapter<MyStationsListRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.fragment_station_list_content, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = position.toString()
+        holder.contentView.text = position.toString()
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.item_number)
+        val contentView: TextView = view.findViewById(R.id.content)
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+}
